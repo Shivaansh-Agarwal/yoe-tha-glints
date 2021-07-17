b5061166961919eac1fa9cf31bab8ddaeb04de94
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-07-17 14:55:40 +0530

    refactor: Moved App.jsx out of components
    Since App.jsx is the entry point of the project, moved it out for better accessibility

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ProfileCard } from "./components";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ProfileCard
+        name="Shivaansh Agarwal"
+        age="11"
+        profileImage="https://via.placeholder.com/150"
+      />
+    </div>
+  );
+};
+
+export default App;
--- src/components/App/App.jsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <div className="App">YOE Project!</div>;
-};
-
-export default App;
