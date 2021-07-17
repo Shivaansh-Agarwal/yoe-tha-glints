aad84fb4b6021396f27225c037f864cca199e7ad
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2021-07-17 16:06:39 +0530

    style: Moved App Styles to it's respective module CSS file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./styles/App.module.css";
 
 const App = () => {
-  return <div className="App">YOE Project!</div>;
+  return <div className={styles.App}>YOE Project!</div>;
 };
 
 export default App;
